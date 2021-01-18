dbe8a1e2cc8593bf09d1a8ef090bd1bab542b750
motoi-tsushima <motoi.tsushima@gmail.com>
2021-01-19 07:31:33 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfHiDraw1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/// <summary>
+/// WPF描画速度検証用テストプログラム
+/// https://www.peliphilo.net/archives/2447
+/// を参考にしました。
+/// </summary>
+namespace WpfHiDraw1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 結果表示用
+            xView.ResultLabel = xLabel;
+        }
+
+        //public List<Tuple<Point, Point>> points = new List<Tuple<Point, Point>>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.StartButton.IsEnabled = false;
+
+            // 適当なシードで乱数を生成する(直線の始点と終点を適当に算出するのに使用)
+            var randSH = new Random(17280489);
+            var randSV = new Random(399594);
+            var randEH = new Random(8793);
+            var randEV = new Random(4498389);
+            var points = new List<Tuple<Point, Point>>();
+            for (var i = 0; i < 10000; ++i)
+            {
+                points.Add(new Tuple<Point, Point>(new Point(randSH.Next(800), randSV.Next(450)), new Point(randEH.Next(800), randEV.Next(450))));
+            }
+            xView.Points = points;
+
+            this.StartButton.IsEnabled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            xView.InvalidateVisual();
+        }
+    }
+}
---
+++ WpfHiDraw1/View.cs
@@ -0,0 +1,127 @@
+﻿//#define DrawingContext_DrawLine
+
+//#define DrawingContext_DrawGeometry_LineTo
+//#define DrawingContext_DrawGeometry_LineTo_OneGeometry
+//#define DrawingContext_DrawGeometry_PolyLineTo_MultiGeometry
+#define DrawingContext_DrawGeometry_PolyLineTo_OneGeometry
+
+//#define DrawingContext_DrawPathGeometry_LineTo
+//#define DrawingContext_DrawPathGeometry_LineTo_OneGeometry
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace WpfHiDraw1
+{
+    public class View : Control
+    {
+        public List<Tuple<Point, Point>> Points;
+        public Label ResultLabel;
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (Points == null || ResultLabel == null)
+            {
+                return;
+            }
+            var pen = new Pen(Brushes.Aqua, 1);
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+#if DrawingContext_DrawLine
+
+            foreach (Tuple<Point, Point> point in Points)
+            {
+                drawingContext.DrawLine(pen, point.Item1, point.Item2);
+            }
+
+#elif DrawingContext_DrawGeometry_LineTo
+
+            foreach (Tuple<Point, Point> point in Points)
+            {
+                var geometry = new StreamGeometry();
+                using (var context = geometry.Open())
+                {
+                    context.BeginFigure(point.Item1, false, false);
+                    context.LineTo(point.Item2, true, false);
+                }
+                drawingContext.DrawGeometry(null, pen, geometry);
+            }
+
+#elif DrawingContext_DrawPathGeometry_LineTo
+
+            foreach (Tuple<Point, Point> point in Points)
+            {
+                PathFigure pointFigure = new PathFigure();
+                pointFigure.StartPoint = new Point(point.Item1.X, point.Item1.Y);
+                pointFigure.Segments.Add(new LineSegment(new Point(point.Item2.X, point.Item2.Y), true));
+
+                var geometry = new PathGeometry();
+                geometry.Figures.Add(pointFigure);
+
+                drawingContext.DrawGeometry(null, pen, geometry);
+            }
+
+#elif DrawingContext_DrawGeometry_LineTo_OneGeometry
+
+            var geometry = new StreamGeometry();
+            using (var context = geometry.Open())
+            {
+                foreach (Tuple<Point, Point> point in Points)
+                {
+                    context.BeginFigure(point.Item1, false, false);
+                    context.LineTo(point.Item2, true, false);
+                }
+            }
+            drawingContext.DrawGeometry(null, pen, geometry);
+
+#elif DrawingContext_DrawPathGeometry_LineTo_OneGeometry
+
+            var geometry = new PathGeometry();
+
+            foreach (Tuple<Point, Point> point in Points)
+            {
+                PathFigure pointFigure = new PathFigure();
+                pointFigure.StartPoint = new Point(point.Item1.X, point.Item1.Y);
+                pointFigure.Segments.Add(new LineSegment(new Point(point.Item2.X, point.Item2.Y), true));
+                geometry.Figures.Add(pointFigure);
+            }
+            drawingContext.DrawGeometry(null, pen, geometry);
+
+#elif DrawingContext_DrawGeometry_PolyLineTo_MultiGeometry
+
+            foreach (Tuple<Point, Point> point in Points)
+            {
+                var geometry = new StreamGeometry();
+                using (var context = geometry.Open())
+                {
+                    context.BeginFigure(point.Item1, false, false);
+                    context.PolyLineTo(new Point[] { point.Item2 }, true, false);
+                }
+                drawingContext.DrawGeometry(null, pen, geometry);
+            }
+
+#elif DrawingContext_DrawGeometry_PolyLineTo_OneGeometry
+
+            var geometry = new StreamGeometry();
+            using (var context = geometry.Open())
+            {
+                foreach (Tuple<Point, Point> point in Points)
+                {
+                    context.BeginFigure(point.Item1, false, false);
+                    context.PolyLineTo(new Point[] { point.Item2 }, true, false);
+                }
+            }
+            drawingContext.DrawGeometry(null, pen, geometry);
+
+#endif
+            sw.Stop();
+            ResultLabel.Content = "Result: " + sw.ElapsedMilliseconds.ToString() + " ms";
+        }
+    }
+}
